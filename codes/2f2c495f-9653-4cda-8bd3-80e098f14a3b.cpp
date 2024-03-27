8efdb728c9cf2fb85a3628f02bc0df30a70b83f4
Frontkick <131354218+Frontkick@users.noreply.github.com>
2024-03-27 14:05:45 +0530

    initial commit

---
+++ codes/2f2c495f-9653-4cda-8bd3-80e098f14a3b.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+vaid main()
+{
+  printf("Hello World!");
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const {generateFile} = require('./generateFile.js');
+const {execute} = require("./execute.js");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/", (req,res) => {
+    return res.json({hello: "world"});
+});
+
+app.post("/run", async (req, res) => {
+    const { language = "cpp", code} = req.body;
+
+    if (code === undefined) {
+        return res.status(400).json({ success: false, error: "Empty code"});
+    }
+
+    try {
+            const filepath = await generateFile(language, code)
+
+            const output = await execute(filepath);
+
+            console.log({output});
+            return res.json({ filepath, output });
+            
+    }
+    catch(err){
+        res.status(500).json({err});
+    }
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!`);
+});
+
